74732c3cedb929e4dc409e2e593adbe6d2fc7d3a
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-02-18 15:50:35 +0500

    init tabs javascript version

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import useCachedResources from "./hooks/useCachedResources";
+import Navigation from "./navigation/StackNavigator";
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation />
+      </SafeAreaProvider>
+    );
+  }
+}
